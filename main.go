61976431ddf887413f4da3cd4d9eb22ac8529ffa
Matthew Rea <mrea@harbortouch.com>
2015-03-21 00:31:59 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"net/http"
+	"io/ioutil"
+)
+
+type RequestMethod string
+
+const (
+	GET RequestMethod = "GET"
+	HEAD = "HEAD"
+	PUT = "PUT"
+	POST = "POST"
+	OPTIONS = "OPTIONS"
+)
+
+type Request struct {
+	URL     string
+	Headers map[string]string
+	Body    []byte
+	Method  RequestMethod
+}
+
+func New(url string) *Request {
+
+	r & Request{
+		URL: url,
+		Body: nil,
+	}
+
+	return r
+}
+
+func (r *Request) Method(method RequestMethod) *Request {
+
+	if method 
+
+	r.Method = method
+
+	return r
+}
+
+func (r *Request) Headers(h map[string]string) *Request {
+	r.Headers = h
+
+	return r
+}
+
+func (r *Request) Map(i interface{}) error {
+
+
+}
+
+func (r *Request) Do() ([]byte, error) {
+
+	cli := &http.Client{}
+	req, err := http.NewRequest(r.Method, r.URL, r.Body)
+	if err != nil {
+		return nil, error
+	}
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return nil, error
+	}
+
+	return body, nil
+}
